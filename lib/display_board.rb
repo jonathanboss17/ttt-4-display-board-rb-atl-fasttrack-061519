2b2c2f309d18443ce93a6f793379bd55b3ac7a6d
dynamic-router-3131 <jtboss47@gmail.com>
2019-06-16 17:27:33 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -58,12 +58,14 @@ def display_board(board)
     print " " + board[0] + " | " + board[1] + " | " + board[2] + " "
     puts ""
     print "-----------"
-    puts""
+    puts ""
+    
     print " " + board[3] + " | " + board[4] + " | " + board[5] + 
      
     puts ""
     print"-----------"
     puts ""
+    
     print " " + board[6] + " | " + board[7] + " | " + board[8] + " "
 end
 display_board(board)
