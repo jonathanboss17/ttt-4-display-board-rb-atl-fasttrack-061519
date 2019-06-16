60202fd10e7090c37684c28708ee769035520c2d
dynamic-router-3131 <jtboss47@gmail.com>
2019-06-16 17:26:58 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -56,9 +56,13 @@ board = ["X","O","X","O","X","O","X","O","X"]
 
 def display_board(board)
     print " " + board[0] + " | " + board[1] + " | " + board[2]
-    puts "-----------"
+    puts ""
+    print "-----------"
+    puts""
     print " " + board[3] + " | " + board[4] + " | " + board[5]
-    puts"-----------"
+    puts ""
+    print"-----------"
+    puts ""
     print " " + board[6] + " | " + board[7] + " | " + board[8]
 end
 display_board(board)
